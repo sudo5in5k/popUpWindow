add633d3dc096718ef1653d7ff64b786b5d0a6a9
Ussy <sushikub@yahoo-corp.jp>
2019-02-05 11:28:44 +0900

    add rx impl

--- app/src/main/java/com/example/sushikub/popupwindow/MainActivity.kt
+++ app/src/main/java/com/example/sushikub/popupwindow/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.sushikub.popupwindow
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,18 +15,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        showPopUpUsingRx(PopUp(this))
+
         popup_container.setOnClickListener {
-            popUp = PopUp(this)
-            showPopUp(popUp)
+            showPopUpUsingButton(PopUp(this))
         }
     }
 
-    private fun showPopUp(popUp: PopUp) {
+    private fun showPopUpUsingButton(popUp: PopUp) {
         if (popUp.isShowing) {
             popUp.dismiss()
             popUp.show(contact_button)
         } else {
             popUp.show(contact_button)
         }
+    }
+
+    private fun showPopUpUsingRx(popUp: PopUp) {
+        Completable.complete()
+                .delay(5, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnComplete { popUp.show(contact_button) }
+                .subscribe()
     }
 }
